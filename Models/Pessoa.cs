83dea67cc111a31bfec264fd489c98948a68aa94
caioandre182 <caioserralvo182@gmail.com>
2022-09-01 16:32:24 -0300

    criando as classes Pessoa e Suite

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trilha_net_explorando_desafio_hotelaria.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ Models/Suite.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trilha_net_explorando_desafio_hotelaria.Models
+{
+    public class Suite
+    {
+        public string TipoSuite  { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
